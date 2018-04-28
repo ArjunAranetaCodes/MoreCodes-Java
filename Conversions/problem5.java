0174e716de6550de036e0e74909e0fc2c681fd34
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-04-28 08:39:18 +0800

    2018 Updates

---
+++ Conversions/problem5.java
@@ -0,0 +1,8 @@
+//Problem 5: Write a program that converts an array/list to string.
+public class Problem5{
+ public static void main(String[]args){
+  String[] arrNumbers = {"1", "2", "3"};
+  String numbers = String.join("",arrNumbers);
+  System.out.println(numbers);
+ }
+}
