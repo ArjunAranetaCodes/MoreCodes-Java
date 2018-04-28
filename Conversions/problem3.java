6963b337183448dbc2483063f04df733ec33164d
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-04-28 08:38:46 +0800

    2018 Updates

---
+++ Conversions/problem3.java
@@ -0,0 +1,7 @@
+//Problem 3: Write a program that converts an integer to float.
+public class Problem3{
+ public static void main(String[]args){
+  int num = 10;
+  System.out.println((float)num);
+ }
+}
