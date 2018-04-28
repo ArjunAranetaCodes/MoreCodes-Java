44836bc43b4503602d7266492f6ead27fc038813
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-04-28 08:38:23 +0800

    2018 Updates

---
+++ Conversions/problem2.java
@@ -0,0 +1,7 @@
+//Problem 2: Write a program that converts a string to integer.
+public class Problem2{
+  public static void main(String[]args){
+    String num = "10";
+    System.out.println(Integer.valueOf(num));
+  }
+}
