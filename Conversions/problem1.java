d929c249b3f28b3a561a7c37e7027580793e620e
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-04-28 08:38:01 +0800

    2018 Updates

---
+++ Conversions/problem1.java
@@ -0,0 +1,7 @@
+//Problem 1: Write a program that converts a number to string.
+public class Problem1{
+  public static void main(String[]args){
+    int num = 10;
+    System.out.println(String.valueOf(num));
+  }
+}
