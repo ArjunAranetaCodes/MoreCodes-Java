8b6fa96964d82aa75bff7b058e849a888a4d4bb2
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:09:38 +0800

    2018 Updates

---
+++ Conversions/problem12.java
@@ -0,0 +1,8 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+public class Problem12{
+ public static void main(String[]args){
+  int day = 1;
+  int seconds = day * 24 * 60 * 60;
+  System.out.println(seconds);
+ }
+}
