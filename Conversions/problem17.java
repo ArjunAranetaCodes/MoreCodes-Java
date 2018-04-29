d5a55cb8fd7794feba67c789d00ba5f49397389f
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:11:20 +0800

    2018 Updates

---
+++ Conversions/problem17.java
@@ -0,0 +1,8 @@
+//Problem 17: Write a program that converts kilometer to meter.
+public class Problem17{
+ public static void main(String[]args){
+  int kilometer = 1;
+  int meter = kilometer * 1000;
+  System.out.println(meter);
+ }
+}
