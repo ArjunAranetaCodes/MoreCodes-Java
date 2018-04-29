3ae3796b3a8a683062f33391f570a23cc48d70f5
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:10:56 +0800

    2018 Updates

---
+++ Conversions/problem16.java
@@ -0,0 +1,8 @@
+//Problem 16: Write a program that converts feet to inch.
+public class Problem16{
+ public static void main(String[]args){
+  int feet = 1;
+  int inches = feet * 12;
+  System.out.println(inches);
+ }
+}
