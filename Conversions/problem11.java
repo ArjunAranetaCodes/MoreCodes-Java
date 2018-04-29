2c050609ee907aeea93a6b26093b3a5c3e864443
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:09:18 +0800

    2018 Updates

---
+++ Conversions/problem11.java
@@ -0,0 +1,8 @@
+//Problem 11: Write a program that converts numbers of day to minutes.
+public class Problem11{
+ public static void main(String[]args){
+  int day = 1;
+  int minutes = day * 24 * 60;
+  System.out.println(minutes);
+ }
+}
