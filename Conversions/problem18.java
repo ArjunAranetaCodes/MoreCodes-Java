0e5a59c9bb43250857201e551648dfab61c3f567
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:11:35 +0800

    2018 Updates

---
+++ Conversions/problem18.java
@@ -0,0 +1,8 @@
+//Problem 18: Write a program that converts liter to milliliter.
+public class Problem18{
+ public static void main(String[]args){
+  int liter = 1;
+  int milliliter = liter * 1000;
+  System.out.println(milliliter);
+ }
+}
