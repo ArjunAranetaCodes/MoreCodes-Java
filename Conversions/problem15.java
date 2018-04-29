07ef40cf8599db1c62dfcd0a6250a97230f248c4
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:10:40 +0800

    2018 Updates

---
+++ Conversions/problem15.java
@@ -0,0 +1,8 @@
+//Problem 15: Write a program that converts letters to ASCII.
+public class Problem15{
+ public static void main(String[]args){
+  char letter = 'x';
+  int ascii = (int)letter;
+  System.out.println(ascii);
+ }
+}
