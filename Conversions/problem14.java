0406f9a30e8bccd631d1b2a0d48c687aee90655e
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-30 07:10:21 +0800

    2018 Updates

---
+++ Conversions/problem14.java
@@ -0,0 +1,7 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+public class Problem14{
+ public static void main(String[]args){
+  String str = "abc";
+  System.out.println(str.toUpperCase());
+ }
+}
