3f5bd5e4ed4dfadbec299a80ad72d791734cf1d0
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-11-07 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem5.java
@@ -0,0 +1,22 @@
+/*
+Problem 5: Write a program to print the number pattern of ones and zeros using loop.
+11111
+00000
+00000
+00000
+11111
+*/
+public class Problem{
+ public static void main(String[] args){
+  for(int y = 0; y < 5; y++){
+   for(int x = 0; x < 5; x++){
+    if((y == 0) || (y == 4)){
+     System.out.print("1");
+    }else{
+     System.out.print("0");
+    }			
+   }
+   System.out.print("\n");
+  }
+ }
+}
