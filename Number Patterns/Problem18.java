02d79dd78b8c85cdc806988913fd5ad3b835e718
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-11-08 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem18.java
@@ -0,0 +1,37 @@
+/*
+Problem 18: Write a program to print the number pattern using loop.
+  1  
+ 111
+11111
+ 111
+  1
+*/
+public class Problem{
+ public static void main(String[] args){
+		int y = 0;
+		int x = 0;
+		int rows = 3;
+		int stars = 1; 
+		int blank = rows - 1;
+
+		for(y=1; y<rows*2; y++){
+			for(x=1; x<=blank; x++){
+				System.out.print(" ");
+			}
+
+			for(x=1; x<stars*2; x++){
+				System.out.print("*");
+			}
+
+			System.out.print("\n");
+
+			if(y<rows){
+				blank--;
+				stars++;
+			}else{
+				blank++;
+				stars--;
+			}
+		}
+ }
+}
