6e6d9c48ce835cbaec98524461fc7fbde635f142
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-11-08 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem11.java
@@ -0,0 +1,19 @@
+/*
+Problem 11: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+*/
+public class Problem{
+ public static void main(String[] args){
+	int row = 5;
+	for(int y = 0; y <= row; y++){
+		for(int x = 0; x < y; x++){
+			System.out.print("*");
+		}
+		System.out.print("\n");
+	}
+ }
+}
